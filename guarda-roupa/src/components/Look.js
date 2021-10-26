323d5e358ff1244d4913ea03a9fa66659ac7636d
Blade <and.santos123@gmail.com>
2021-10-26 16:02:20 -0300

    ajuste

--- guarda-roupa/src/components/Look.js
+++ guarda-roupa/src/components/Look.js
@@ -4,13 +4,13 @@ export default class Look extends React.Component{
         return(
             <div>
           <div className="shirt">
-            {this.props.shirt === null ? <p>Selecione uma camisa!</p> : <img src={this.props.shirt} alt="Camisa" style={{width: 50, height: 50}} />}
+            {this.props.shirt === null ? <p>Selecione uma Camisa!</p> : <img src={this.props.shirt} alt="Camisa" style={{width: 50, height: 50}} />}
           </div>
           <div className="pant">
-            {this.props.pant === null ? <p>Selecione uma calça!</p> : <img src={this.props.pant} alt="Calça" style={{width: 50, height: 50}} />}
+            {this.props.pant === null ? <p>Selecione uma Calça!</p> : <img src={this.props.pant} alt="Calça" style={{width: 50, height: 50}} />}
           </div>
           <div className="sneaker">
-            {this.props.sneaker === null ? <p>Selecione um tênis!</p> : <img src={this.props.sneaker} alt="Tênis" style={{width: 50, height: 50}} />}
+            {this.props.sneaker === null ? <p>Selecione um Tênis!</p> : <img src={this.props.sneaker} alt="Tênis" style={{width: 50, height: 50}} />}
           </div>
         </div>
         )
